89cde002f25a739b8e1289bad2329ce4cef6ce13
mark <csiitg82@gmail.com>
2019-02-14 19:27:49 +0800

    home carousel 全部搞定

--- js/action.js
+++ js/action.js
@@ -1,8 +1,35 @@
 $(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
+
+    $('.owl_feature').owlCarousel({
         loop:true,
         margin:10,
-        nav:true,
+        nav:false,
+        dots:false,
+        autoplay:true,
+    autoplayTimeout:3600,
+    autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:4
+            }
+        }
+    })
+
+    $('.owl_best').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots:false,
+        autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+
         responsive:{
             0:{
                 items:1
@@ -18,16 +45,41 @@ $(document).ready(function(){
 
     })
 
-    var owl = $('.owl-carousel');
-    owl.owlCarousel();
 
-    $('.owl_prev_bt').click(function(e){
-        owl.trigger('prev.owl.carousel', [300]);
+    // feature control
+    var owl1 = $('.owl_feature');
+
+
+    $('.feature_bt_prev').click(function(e){
+        owl1.trigger('prev.owl.carousel', [300]);
         
     })
-    $('.owl_next_bt').click(function() {
-        owl.trigger('next.owl.carousel');
+    $('.feature_bt_next').click(function() {
+        owl1.trigger('next.owl.carousel');
     })
+    $('.latest_bt').click(function(e){
+        owl1.trigger('next.owl.carousel');
+    })
+    $('.best_seller_bt').click(function(e){
+        owl1.trigger('prev.owl.carousel', [300]);
+    })
+
+    
+    // best control
+
+    var owl2 = $('.owl_best');
+
+
+    $('.best_bt_prev').click(function(e){
+        owl2.trigger('prev.owl.carousel', [300]);
+        
+    })
+    $('.best_bt_next').click(function() {
+        owl2.trigger('next.owl.carousel');
+    })
+   
+
+
 
 
 
